5ef5fae00ff83b6a2a4c6d2bff43b491b4dc6d2e
Kedap <kedap.dev@protonmail.com>
2024-06-05 02:59:18 -0600

    feat(pokelogica/Pokemon): agregando clase sin logica

---
+++ pokeLogica/src/main/java/org/codedestroyers/pokejuego/Pokemon.java
@@ -0,0 +1,37 @@
+package org.codedestroyers.pokejuego;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import org.codedestroyers.pokejuego.posiones.Posion;
+
+public class Pokemon {
+    private String nombre;
+    private int vida;
+    private @Setter(AccessLevel.PUBLIC) Posion posion;
+    private boolean posionActiva;
+    private int danoGolpe;
+    private int defensa;
+    private @Getter(AccessLevel.PUBLIC) String imagenSeleccionada;
+    private @Getter(AccessLevel.PUBLIC) String imagenBatalla;
+
+    //TODO: Constructor
+
+    public int golpear(Pokemon p) {
+        //TODO
+        return 0;
+    }
+
+    public int recibirGolpe(int dano) {
+        //TODO
+        return 0;
+    }
+
+    public void tomarPosion() {
+        //TODO
+    }
+
+    public boolean haPerdido() {
+        return vida <= 0;
+    }
+}
