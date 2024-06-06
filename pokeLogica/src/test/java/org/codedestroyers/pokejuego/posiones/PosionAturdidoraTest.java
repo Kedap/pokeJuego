919a7ffc46375c2c9b153fd116c49ca5a0540ce4
Kedap <kedap.dev@protonmail.com>
2024-06-05 23:24:20 -0600

    tests(pokelogica/Posiones): agregando tests para las posiones

--- pokeLogica/src/test/java/org/codedestroyers/pokejuego/posiones/PosionAturdidoraTest.java
+++ pokeLogica/src/test/java/org/codedestroyers/pokejuego/posiones/PosionAturdidoraTest.java
@@ -1,18 +1,16 @@
 package org.codedestroyers.pokejuego.posiones;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PosionOrdinariaTest {
+class PosionAturdidoraTest {
 
     @ParameterizedTest
     @CsvSource({"100.0,90.0,10.0", "50.0,50.0,25.0", "10.0,20.0,8.0"})
     void obtenerDanoNuevo(double danoNomral, double danoPosion, double danoEsperado) {
-        PosionOrdinaria po = new PosionOrdinaria("Posion Ordinaria", 5, (int) danoPosion);
+        PosionAturdidora po = new PosionAturdidora("Posion Ordinaria", 5, (int) danoPosion,30);
         assertEquals(po.obtenerDanoNuevo(danoNomral), danoEsperado);
     }
 }
--- pokeLogica/src/test/java/org/codedestroyers/pokejuego/posiones/PosionCongeladoraTest.java
+++ pokeLogica/src/test/java/org/codedestroyers/pokejuego/posiones/PosionCongeladoraTest.java
@@ -1,18 +1,16 @@
 package org.codedestroyers.pokejuego.posiones;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PosionOrdinariaTest {
+class PosionCongeladoraTest {
 
     @ParameterizedTest
     @CsvSource({"100.0,90.0,10.0", "50.0,50.0,25.0", "10.0,20.0,8.0"})
     void obtenerDanoNuevo(double danoNomral, double danoPosion, double danoEsperado) {
-        PosionOrdinaria po = new PosionOrdinaria("Posion Ordinaria", 5, (int) danoPosion);
+        PosionCongeladora po = new PosionCongeladora("Posion Ordinaria", 5, (int) danoPosion,30);
         assertEquals(po.obtenerDanoNuevo(danoNomral), danoEsperado);
     }
 }
